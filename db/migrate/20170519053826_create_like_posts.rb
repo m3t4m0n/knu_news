3662d4cf8184948050181711bb6e94e378823b91
soomin <soomti@likelion.org>
2017-05-19 06:32:03 +0000

    toss hyo

--- db/migrate/20170519053826_create_like_posts.rb
+++ db/migrate/20170519053826_create_like_posts.rb
@@ -2,8 +2,6 @@ class CreateLikePosts < ActiveRecord::Migration
   def change
     create_table :like_posts do |t|
       
-      t.integer poster_id
-      t.integer user_id
 
       t.timestamps null: false
     end
